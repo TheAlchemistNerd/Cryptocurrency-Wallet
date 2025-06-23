918142cc308f666bc4c9f7e46113a3ed32a80e15
TheAlchemistNerd <nevillemaloba@yahoo.com>
2025-06-23 16:17:28 +0300

    updated CreateWalletRequestDTO and SendTransactionRequestDTO with constraint validation

--- src/main/java/com/cryptowallet/dto/CreateWalletRequestDTO.java
+++ src/main/java/com/cryptowallet/dto/CreateWalletRequestDTO.java
@@ -1,5 +1,8 @@
 package com.cryptowallet.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateWalletRequestDTO(
+    @NotBlank(message = "userId must not be blank")
     String userId
 ) {}
--- src/main/java/com/cryptowallet/dto/SendTransactionRequestDTO.java
+++ src/main/java/com/cryptowallet/dto/SendTransactionRequestDTO.java
@@ -1,7 +1,17 @@
 package com.cryptowallet.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public record SendTransactionRequestDTO (
+    @NotBlank(message = "senderWalletId must not be blank")
     String senderWalletId,
+    @NotBlank(message = "receiverAddress must not be blank")
     String receiverAddress,
+    @Positive(message = "amount must be positive")
     double amount
-){}
+){
+    public String generateIdempotencyKey() {
+       return senderWalletId + ":" + receiverAddress + ":" + amount;
+    }
+}
